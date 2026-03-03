9530f58408194f09623d584776c52e9e64493ddd
VladislavDzyuba <vladoalo@gmail.com>
2026-03-03 17:39:18 +0400

    Update first_script.sh

--- first_script.sh
+++ first_script.sh
@@ -1,1 +1,7 @@
 neofetch
+
+Добавьте GPG-ключ: curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg.
+Добавьте репозиторий: echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null.
+Снова обновите список пакетов: sudo apt update.
+Установите Docker: sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin. 
+docs.docker.com +1
